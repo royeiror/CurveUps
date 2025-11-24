995535da52b33a5c4dbc36925a506181ece76f0c
royeiror <royeiror@gmail.com>
2025-11-24 17:17:44 -0600

    Delete src/hook-curveup.py

--- src/hook-curveup.py
+++
@@ -1,5 +0,0 @@
-# hook-curveup.py - PyInstaller hook file
-from PyInstaller.utils.hooks import collect_all
-
-# Force include all curveup modules
-datas, binaries, hiddenimports = collect_all('curveup')
