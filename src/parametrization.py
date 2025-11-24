86108f347f39a55016b4ad5cfa42829ec6718c71
royeiror <royeiror@gmail.com>
2025-11-24 17:25:27 -0600

    Update parametrization.py

--- src/parametrization.py
+++ src/parametrization.py
@@ -1,7 +1,3 @@
-# src/parameterization.py
 class MeshParameterizer:
     def __init__(self, mesh):
         self.mesh = mesh
-    
-    def conformal_parameterization(self):
-        return "Conformal parameterization complete"
